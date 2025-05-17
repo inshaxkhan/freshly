458f822305724f154d67de2e5ea6b0db92d4794a
Insha Khan <khaninsha0001@gmail.com>
2025-05-17 23:57:46 +0530

    made changes to home page, added code to app context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,15 +1,36 @@
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {dummyProducts } from "../assets/assets"
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) =>{
+
+    const currency=import.meta.VITE_CURRENCY;
     const navigate = useNavigate();
     const [user, setUser]= useState(null);
     const [isSeller, setIsSeller]= useState(false);
     const [showUserLogin, setShowUserLogin]= useState(false);
+    const [products, setProducts]= useState([]);
+    const [cartItems, setCartItems]= useState([]);
 
-    const value={ navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin}
+    //fetch all products
+    const fetchProducts=async()=>{
+        setProducts( dummyProducts )
+    }
+
+    //add products to cart
+    const addToCart=()=>{
+        let cardData=structuredClone(cardItems);
+        
+    }
+
+
+    useEffect(()=>{
+        fetchProducts()
+    },[])
+
+    const value={ navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency}
     
     return <AppContext.Provider value={value}>
         {children}
