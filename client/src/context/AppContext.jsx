478d243093fc6faea8e1554797e4a27a2ba358be
Insha Khan <khaninsha0001@gmail.com>
2025-05-20 00:29:19 +0530

    update all products page, add search functionality

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -12,8 +12,10 @@ export const AppContextProvider = ({children}) =>{
     const [user, setUser]= useState(null);
     const [isSeller, setIsSeller]= useState(false);
     const [showUserLogin, setShowUserLogin]= useState(false);
+
     const [products, setProducts]= useState([]);
-    const [cartItems, setCartItems]= useState([]);
+    const [cartItems, setCartItems]= useState({});
+    const [searchQuery, setSearchQuery]= useState({});
 
     //fetch all products
     const fetchProducts=async()=>{
@@ -57,7 +59,7 @@ export const AppContextProvider = ({children}) =>{
         fetchProducts()
     },[])
 
-    const value={ navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems}
+    const value={ navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery}
     
     return <AppContext.Provider value={value}>
         {children}
