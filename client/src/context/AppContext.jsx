85327099a81547f938c42d1b7de7395ac7a71265
Insha Khan <khaninsha0001@gmail.com>
2025-05-27 23:59:42 +0530

    add cart functionality in nav, create cart page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -56,11 +56,37 @@ export const AppContextProvider = ({children}) =>{
         setCartItems(cartData)
     }
 
+
+// get cart item count
+const getCartCount=()=>{
+    let totalCount=0;
+    for(const item in cartItems){
+        totalCount+=cartItems[item];
+    }
+    return totalCount;
+}
+
+// get cart total amount
+const getCartAmount=()=>{
+    let totalAmount=0;
+    for(const items in cartItems){
+        let itemInfo=products.find((product)=>product._id===items);
+        if(cartItems[items]>0){
+            totalAmount+=itemInfo.offerPrice * cartItems[items]
+        }
+    }
+
+    return Math.floor(totalAmount*100)/100;
+}
+
     useEffect(()=>{
         fetchProducts()
     },[])
 
-    const value={ navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery}
+    const value={ navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, 
+        products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery,
+        getCartCount, getCartAmount
+    }
     
     return <AppContext.Provider value={value}>
         {children}
