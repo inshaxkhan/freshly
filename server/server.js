aefe72f3494e6da7f2590d7f7a86c12d9436cc62
Insha Khan <khaninsha0001@gmail.com>
2025-06-02 23:59:43 +0530

    create folder structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from 'cors';
 
 const app=express();
 const port=process.env.PORT || 4000;
 
-const allowedOrigins=['https://localhost:5173']
+//Allow multiple origins
+const allowedOrigins=['http://localhost:5173']
 
 // middleware config
 app.use(express.json());
