9817022761322c332a67672641f48a8914b93a13
Insha Khan <khaninsha0001@gmail.com>
2025-06-07 23:39:22 +0530

    create api endpoint for authUser & logout

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { register } from "../controllers/userController";
+import authUser from "../middlewares/authUser";
 
 const userRouter=express.Router();
 
 userRouter.post('/register', register)
+userRouter.post('/login', login)
+userRouter.post('/is-auth', authUser, isAuth)
+userRouter.post('/logout', authUser, logout)
 
 export default userRouter;
