39e864a3710c9ca31f4e450fbbfda14438868759
Insha Khan <khaninsha0001@gmail.com>
2025-06-08 23:51:32 +0530

    create middleware for seller authentication, and controller function sellerController

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+//Login seller: /api/seller/login
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      password === process.env.SELLER_PASSWORD &&
+      email === process.env.SELLER_EMAIL
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("sellerToken", token, {
+        httpOnly: true, // prevent js to access cookie
+        secure: process.env.NODE_ENV === "production", //use secure cookie in prod
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", //CSRF protection
+        maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiration time
+      });
+      return res.json({ success: true, message: "Logged In" });
+    } else {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch {
+    console.log(error.message);
+    res.json({success:false, message:error.message});
+  }
+};
