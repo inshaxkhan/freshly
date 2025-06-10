292f2b0bbbc412e64c2b2df5bf04b673c183d1ac
Insha Khan <khaninsha0001@gmail.com>
2025-06-11 00:05:39 +0530

    feat: add seller logout, and auth check with JWT and secure cookies

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -28,3 +28,31 @@ export const sellerLogin = async (req, res) => {
     res.json({success:false, message:error.message});
   }
 };
+
+
+//Seller is-auth : /api/seller/is-auth 
+
+export const isSellerAuth=async(req, res )=>{
+    try {
+        return res.json({success:true});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message});
+    }
+}
+
+//logout seller : /api/seller/logout
+export const sellerLogout=async(req,res)=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly:true,
+            secure:process.env.NODE_ENV==='production',
+            sameSite:process.env.NODE_ENV==='production'?'none':'strict', 
+        })
+
+        return res.json({success:true, message: "Logged Out"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message});
+    }
+}
