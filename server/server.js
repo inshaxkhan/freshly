42307287cb2f1143011193c73a3a329b13b356f8
Insha Khan <khaninsha0001@gmail.com>
2025-06-15 23:17:33 +0530

    feat: add product upload with image support via Cloudinary and Multer

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ import connectDB from "./configs/db.js";
 import "dotenv/config"
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 const app=express();
 const port=process.env.PORT || 4000;
 
 //mongoDB
 await connectDB();
+
+//cloudinary
+await connectCloudinary();
 
 //Allow multiple origins
 const allowedOrigins=['http://localhost:5173']
