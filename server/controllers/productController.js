88b4121d50e17c235145133324ebd96af79191cc
Insha Khan <khaninsha0001@gmail.com>
2025-06-16 23:54:38 +0530

    feat: add product listing, single product retrieval, and stock update APIs

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -24,16 +24,36 @@ export const addProduct=async(req, res)=>{
 
 // Get product: /api/product/list
 export const productList=async(req, res)=>{
-    
+    try {
+        const products=await Product.find({})
+        res.json({success:true, products})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
 }
 
 // Get single product: /api/product/id
 export const productById=async(req, res)=>{
-    
+    try {
+        const {id}=req.body
+        const product =await Product.findById(id)
+        res.json({success:true, product})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
 }
 
 
 // Change product in stock: /api/product/stock
 export const changeStock=async(req, res)=>{
-    
+    try {
+        const {id, inStock }=req.body
+        await Product.findByIdAndUpdate(id, {inStock})
+        res.json({success:true, message:"Stock Updated"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
 }
