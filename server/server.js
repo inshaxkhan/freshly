455cf6fcf8ea59203938d2695a91a128cc191f2a
Insha Khan <khaninsha0001@gmail.com>
2025-06-17 23:56:23 +0530

    feat: add pr0oduct upload and user cart update functionality

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import "dotenv/config"
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
 
 const app=express();
 const port=process.env.PORT || 4000;
@@ -30,6 +31,7 @@ app.get("/", (req,res)=>{
 
 app.use('/api/user', userRouter);
 app.use('/api/seller', sellerRouter);
+app.use('/api/product', productRouter);
 
 app.listen(port, ()=>{
     console.log(`Server running on https://localhost:${port}`);
