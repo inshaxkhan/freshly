ee00fa563fdcbaa512706ce024aea825c564aefa
Insha Khan <khaninsha0001@gmail.com>
2025-06-19 23:17:19 +0530

    create controller function to get individual order data

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -27,8 +27,28 @@ export const placeOrderCOD= async(req, res)=>{
             paymentType:"COD",
         });
 
-        return res.json({success:true, message:"Order placed successfully"})
+        return res.json({success:true, message:"Order placed successfully "})
     } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+// get orders by userId: /api/order/user
+export const getUserOrders=async(req,res)=>{
+    try {
+        const {userId}=req.body
+
+        //display when 'order is COD' or 'order is paid online'
+        const orders=await Order.find({
+            userId,
+            $or:[{paymentType:"COD"}, {isPaid: true}]
+        }).populate("items.product address").sort({createdAt:-1})
+
+        res.json({success:true, orders})
+
+    } catch (error) { 
         console.log(error.message)
         res.json({success:false, message:error.message})
     }
