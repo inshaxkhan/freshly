f37a90c65ec09054c0f86613319e97c40a27605c
Insha Khan <khaninsha0001@gmail.com>
2025-06-20 23:53:41 +0530

    feat(order): Add route and controller for fetching all seller/admin orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -53,3 +53,21 @@ export const getUserOrders=async(req,res)=>{
         res.json({success:false, message:error.message})
     }
 }
+
+// get all orders for ADMIN/SELLER: /api/order/seller
+export const getAllOrders=async(req,res)=>{
+    try { 
+        //display when 'order is COD' or 'order is paid online'
+        const orders=await Order.find({
+            $or:[{paymentType:"COD"}, {isPaid: true}]
+        }).populate("items.product address").sort({createdAt:-1});
+
+        res.json({success:true, orders});
+
+    } catch (error) { 
+        console.log(error.message)
+        res.json({success:false, message:error.message})
+    }
+}
+
+
