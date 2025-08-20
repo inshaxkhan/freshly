da2741a1a2374bf6a337680729dcc25fede48551
Insha Khan <khaninsha0001@gmail.com>
2025-08-21 00:39:51 +0530

    fixed a ton of bugs in ADD & GET ADDRESS

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
- const addressSchema=mongoose.Schema({
+ const addressSchema= new mongoose.Schema({
     userId:{type:String, required:true},
     firstName:{type:String, required:true},
     lastName:{type:String, required:true},
@@ -13,6 +13,6 @@ import mongoose from "mongoose";
     phone:{type:String, required:true}
 })
 
- const Address=mongoose.model.address || mongoose.model('address', addressSchema);
+ const Address=mongoose.models.address || mongoose.model('address', addressSchema);
 
  export default Address
